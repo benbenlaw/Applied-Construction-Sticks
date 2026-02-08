f64161d9630e3e9f3c18c71f2fb8245f9d92b1e0
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-02-08 09:12:06 +0000

    1.0.5

--- src/main/java/com/benbenlaw/appliedsticks/mixin/RenderBlockPreviewMixin.java
+++ src/main/java/com/benbenlaw/appliedsticks/mixin/RenderBlockPreviewMixin.java
@@ -7,6 +7,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+//DISABLING THIS AT THE MOMENT
+
 @Mixin(RenderBlockPreview.class)
 public class RenderBlockPreviewMixin {
 
