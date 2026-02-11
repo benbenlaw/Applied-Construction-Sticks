8757b9ffa4f3dffca96bf2b354113dc43624c731
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-02-11 18:52:16 +0000

    1.1.1

--- src/main/java/com/benbenlaw/appliedsticks/AppliedSticks.java
+++ src/main/java/com/benbenlaw/appliedsticks/AppliedSticks.java
@@ -2,11 +2,13 @@ package com.benbenlaw.appliedsticks;
 
 import com.benbenlaw.appliedsticks.containers.handlers.AppliedSticksContainerRegistrar;
 import com.benbenlaw.appliedsticks.integration.AE2Integration;
+import com.benbenlaw.appliedsticks.network.AppliedSticksMessages;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,8 +18,13 @@ public class AppliedSticks{
     public static final String MOD_ID = "appliedsticks";
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public AppliedSticks(final IEventBus eventBus, final ModContainer modContainer) {
+    public AppliedSticks(IEventBus eventBus, ModContainer modContainer) {
+        eventBus.addListener(this::networkingSetup);
         eventBus.addListener(AppliedSticks::commonSetup);
+    }
+
+    public void networkingSetup(RegisterPayloadHandlersEvent event) {
+        AppliedSticksMessages.registerNetworking(event);
     }
 
     public static void commonSetup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/com/benbenlaw/appliedsticks/event/ServerEvents.java
@@ -0,0 +1,57 @@
+package com.benbenlaw.appliedsticks.event;
+
+import appeng.api.ids.AEComponents;
+import com.benbenlaw.appliedsticks.AppliedSticks;
+import com.benbenlaw.appliedsticks.network.StickJobPacket;
+import mrbysco.constructionstick.items.stick.ItemStick;
+import mrbysco.constructionstick.stick.StickJob;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Set;
+
+@EventBusSubscriber(modid = AppliedSticks.MOD_ID)
+public class ServerEvents {
+
+    private static final HashMap<Player, Set<BlockPos>> lastSentPositions = new HashMap<>();
+
+    @SubscribeEvent
+    public static void updateStickJob(PlayerTickEvent.Pre event) {
+        Player player = event.getEntity();
+        Level level = player.level();
+        if (!level.isClientSide()) {
+            ItemStack heldItem = player.getMainHandItem();
+            HitResult result = player.pick(player.blockInteractionRange(), 0.0F, false);
+
+            if (heldItem.has(AEComponents.WIRELESS_LINK_TARGET)
+                    && heldItem.is(TagKey.create(Registries.ITEM, ResourceLocation.parse("constructionstick:construction_sticks")))
+                    && result instanceof BlockHitResult hitResult) {
+
+                StickJob job = ItemStick.getStickJob(player, level, hitResult, heldItem);
+                Set<BlockPos> currentPositions = job.getBlockPositions();
+
+                if (!Objects.equals(lastSentPositions.get(player), currentPositions)) {
+                    lastSentPositions.put(player, currentPositions);
+                    PacketDistributor.sendToPlayer((ServerPlayer) player, new StickJobPacket(currentPositions));
+                }
+            } else {
+                lastSentPositions.remove(player);
+            }
+        }
+    }
+}
