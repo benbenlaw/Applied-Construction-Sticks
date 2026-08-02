267fd6ebc4a3849084afe3771563273d496154e8
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-08-02 12:40:30 +0100

    2.0.2

--- src/main/java/com/benbenlaw/appliedsticks/event/ServerEvents.java
+++ src/main/java/com/benbenlaw/appliedsticks/event/ServerEvents.java
@@ -33,24 +33,35 @@ public class ServerEvents {
     public static void updateStickJob(PlayerTickEvent.Pre event) {
         Player player = event.getEntity();
         Level level = player.level();
-        if (!level.isClientSide()) {
-            ItemStack heldItem = player.getMainHandItem();
-            HitResult result = player.pick(player.blockInteractionRange(), 0.0F, false);
+        if (level.isClientSide()) return;
 
-            if (heldItem.has(AEComponents.WIRELESS_LINK_TARGET)
-                    && heldItem.is(TagKey.create(Registries.ITEM, Identifier.parse("constructionstick:construction_sticks")))
-                    && result instanceof BlockHitResult hitResult) {
+        ItemStack heldItem = player.getMainHandItem();
 
-                StickJob job = ItemStick.getStickJob(player, level, hitResult, heldItem);
-                Set<BlockPos> currentPositions = job.getBlockPositions();
+        boolean isConstructionStick = heldItem.has(AEComponents.WIRELESS_LINK_TARGET)
+                && heldItem.is(TagKey.create(Registries.ITEM, Identifier.parse("constructionstick:construction_sticks")));
 
-                if (!Objects.equals(lastSentPositions.get(player), currentPositions)) {
-                    lastSentPositions.put(player, currentPositions);
-                    PacketDistributor.sendToPlayer((ServerPlayer) player, new StickJobPacket(currentPositions));
-                }
-            } else {
-                lastSentPositions.remove(player);
+        if (!isConstructionStick) {
+            lastSentPositions.remove(player);
+            return;
+        }
+
+        HitResult result;
+        try {
+            result = player.pick(player.blockInteractionRange(), 0.0F, false);
+        } catch (IllegalStateException e) {
+            return;
+        }
+
+        if (result instanceof BlockHitResult hitResult) {
+            StickJob job = ItemStick.getStickJob(player, level, hitResult, heldItem);
+            Set<BlockPos> currentPositions = job.getBlockPositions();
+
+            if (!Objects.equals(lastSentPositions.get(player), currentPositions)) {
+                lastSentPositions.put(player, currentPositions);
+                PacketDistributor.sendToPlayer((ServerPlayer) player, new StickJobPacket(currentPositions));
             }
+        } else {
+            lastSentPositions.remove(player);
         }
     }
 }
