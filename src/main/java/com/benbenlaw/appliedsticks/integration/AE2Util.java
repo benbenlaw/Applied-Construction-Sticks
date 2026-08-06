a95ebc33367b87e1471bcc5082d2bce8da2ff9f4
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-08-06 12:49:20 +0100

    1.21.1

--- src/main/java/com/benbenlaw/appliedsticks/integration/AE2Util.java
+++ src/main/java/com/benbenlaw/appliedsticks/integration/AE2Util.java
@@ -1,23 +1,17 @@
 package com.benbenlaw.appliedsticks.integration;
 
-import appeng.api.AECapabilities;
-import appeng.api.config.Actionable;
 import appeng.api.ids.AEComponents;
 import appeng.api.implementations.blockentities.IWirelessAccessPoint;
 import appeng.api.networking.IGrid;
-import appeng.api.networking.security.IActionSource;
-import appeng.api.stacks.AEItemKey;
 import appeng.api.storage.MEStorage;
 import mrbysco.constructionstick.items.stick.ItemStickBasic;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-
-import java.util.Iterator;
 
 public class AE2Util {
 
@@ -33,8 +27,16 @@ public class AE2Util {
         GlobalPos pos = getLinkedPos(stick);
         if (pos == null) return null;
 
-        Level level = player.level();
-        BlockEntity blockEntity = level.getBlockEntity(pos.pos());
+        if (!(player instanceof ServerPlayer serverPlayer)) return null;
+
+        MinecraftServer server = serverPlayer.getServer();
+
+        ServerLevel targetLevel = server.getLevel(pos.dimension());
+        if (targetLevel == null) return null;
+
+        if (!targetLevel.isLoaded(pos.pos())) return null;
+
+        BlockEntity blockEntity = targetLevel.getBlockEntity(pos.pos());
         if (!(blockEntity instanceof IWirelessAccessPoint accessPoint)) return null;
 
         IGrid grid = accessPoint.getGrid();
